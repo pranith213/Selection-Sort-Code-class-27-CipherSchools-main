85358faa8ef16197098bb24ee33163481db42e3f
pranith213 <126413130+pranith213@users.noreply.github.com>
2024-02-04 22:32:00 +0530

    Add files via upload

---
+++ Selection-Sort-Code-class-27-CipherSchools-main/SelectionSort.java
@@ -0,0 +1,27 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = {3,7,10,11,4,1};
+        int n = arr.length;
+
+        for(int i = 0; i < n-1; i++){
+            //find index of minimum element
+            int minIndex = i;
+            for(int j = i+1; j<n; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            //the index of min element is found in minIndex
+            //now we will swap the min element and bring it in its place
+            //i.e. swap elements at minIndex and i
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        for(int i = 0; i < n; i++){
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
